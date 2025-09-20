836e685ce5a9979eaaba506f506a2154ee56600b
jkumarp <155287978+jkumarp@users.noreply.github.com>
2025-09-20 13:38:02 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
           }
         />
         <Route path="/login" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/register" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/register" element={<Navigate to="/register" replace />} />
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </Layout>
